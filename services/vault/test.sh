ea658665e47c2c7cde6daf7bdc3d0c95c565798f
singhalkarun <singhalkarun@gmail.com>
2023-08-02 05:58:00 +0000

    Automated nginx configuration for vault

--- services/vault/test.sh
+++
@@ -1,6 +0,0 @@
-root_token_text=$(grep "Initial Root Token" initialization-data.txt)
-        if [[ $root_token_text =~ :[[:space:]]*(.*) ]]; then
-                token="${BASH_REMATCH[1]}"
-                echo "export ANSIBLE_HASHI_VAULT_TOKEN=$token" >> ~/.bashrc
-                echo "export ANSIBLE_HASHI_VAULT_ADDR=$VAULT_ADDR" >> ~/.bashrc
-        fi
